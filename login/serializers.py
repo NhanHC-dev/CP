9783ff35a8a1c987fb48ffe4228e7604d6f627dd
NhanHC-dev <nhanhc.dev@gmail.com>
2022-05-25 17:12:39 +0700

    Root

---
+++ login/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from login.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(required=True)
